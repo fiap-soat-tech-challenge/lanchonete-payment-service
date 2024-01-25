12d189c5c1d86954bef6781d68eeaea9020f6436
Jonilson Sousa <jonilsonds9@gmail.com>
2024-01-24 23:21:47 -0300

    Arrumando testes

---
+++ src/usecases/payment.use.cases.spec.ts
@@ -0,0 +1,7 @@
+describe('PaymentUseCases', () => {
+  describe('updateStatus', () => {
+    it('should update status and call production service for approved payment', async () => {
+      expect(true).toEqual(true);
+    });
+  });
+});
