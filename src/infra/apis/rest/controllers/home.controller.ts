b39c2c75885d54d7184e40081659977a93fbe85a
Jonilson Sousa <jonilsonds9@gmail.com>
2024-01-29 18:23:51 -0300

    Ajustes docs routes

--- src/infra/apis/rest/controllers/home.controller.ts
+++ src/infra/apis/rest/controllers/home.controller.ts
@@ -11,7 +11,7 @@ export class HomeController {
       message: 'Bem-vindo ao Payment Service Lanchonete!',
       docs: `${request.protocol}://${request.get('Host')}${
         request.originalUrl
-      }api/docs`,
+      }api/pagamentos/docs`,
     };
   }
 }
