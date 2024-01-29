c94a93c7755ef9155aadfae502f8f68e688474a1
Jonilson Sousa <jonilsonds9@gmail.com>
2024-01-29 20:40:50 -0300

    Fix health check

--- src/infra/health/health.controller.ts
+++ src/infra/health/health.controller.ts
@@ -17,6 +17,6 @@ export class HealthController {
   @Get()
   @HealthCheck()
   check() {
-    return this.health.check([() => this.db.pingCheck('database')]);
+    return this.health.check([() => this.db.pingCheck('database', { timeout: 100000 })]);
   }
 }
